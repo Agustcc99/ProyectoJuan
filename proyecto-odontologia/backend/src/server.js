11f15dedacc86f79c2b2aa78a7c0c72832eb8ea8
Agustcc99 <169953599+Agustcc99@users.noreply.github.com>
2026-04-16 12:05:58 -0300

    Primero Estructura

---
+++ proyecto-odontologia/backend/src/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const app = require("./app");
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+  console.log(`Servidor corriendo en http://localhost:${PUERTO}`);
+});
